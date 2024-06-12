a61f430aa3e07456d52d9e4de6c8c46e50833ed9
Lee <lee.mugweru@gmail.com>
2024-06-12 22:05:25 +0300

    Connects tasks.db

--- taskmanager/database.py
+++ taskmanager/database.py
@@ -3,16 +3,3 @@ import sqlite3
 def connect():
     return sqlite3.connect("tasks.db")
 
-def create_tables():
-    with connect() as conn:
-        cursor = conn.cursor()
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS tasks (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT NOT NULL,
-            description TEXT,
-            due_date TEXT,
-            completed BOOLEAN NOT NULL CHECK (completed IN (0, 1))
-        )
-        """)
-        conn.commit()
